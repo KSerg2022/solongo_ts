fe6c97b8dbea0fac3bdb4b3c172aa7e107bd1cf3
KSerg2022 <114860122+KSerg2022@users.noreply.github.com>
2023-11-18 12:23:36 +0100

    created user hook - usePokemons

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,16 @@
-import React, {useEffect, useState} from 'react';
-import axios from "axios";
+import React, {useState} from 'react';
 
 import './css/main.css'
 import Header from "./componants/Header";
 import Pokemons from "./componants/Pokemons";
 import {IPokemons} from "./model";
-import {Pagination} from "./componants/UI/MyPagination/Pagination";
 
-const baseUrl = "https://pokeapi.co/api/v2/pokemon/"
+import {usePokemons} from "./hooks/pokemons"
 
 
 export const App = () => {
-    const [pokemons, setPokemons] = useState<IPokemons[]>([])
-    const [qty, setQty] = useState<number>(16)
+    const {pokemons, qty, setQty} = usePokemons()
     const [limit, setLimit] = useState<number>(10)
-
-    const [totalPages, setTotalPages] = useState<number>(0);
-    const [page, setPage] = useState<number>(1)
 
     const updateLimit = (value: React.SetStateAction<number>) => {
         if (value === -1) {
@@ -25,27 +19,6 @@ export const App = () => {
             setLimit(value)
         }
     }
-
-
-    useEffect(() => {
-        setPage(1)
-    }, [totalPages])
-
-    useEffect(() => {
-        setPokemons(prevState => prevState = [])
-        let start: number = 1
-        for (; start <= qty; start++) {
-            axios.get(baseUrl.concat(String(start))).then((res) => {
-                let data: IPokemons = {
-                    id: res.data.id,
-                    name: res.data.name,
-                    img_url: res.data.sprites.other.home.front_default,
-                    types: res.data.types.map((e: { type: { name: string; }; }) => e.type.name)
-                }
-                setPokemons(prevState => [...prevState, data])
-            })
-        }
-    }, [qty])
 
     const sortedPokemons = (): IPokemons[] => {
         return pokemons.sort(function (a: IPokemons, b: IPokemons) {
@@ -65,14 +38,8 @@ export const App = () => {
             <main>
                 <Pokemons
                     limit={limit}
-                    page={page}
-                    setPage={setPage}
-                    totalPages={totalPages}
-                    setTotalPages={setTotalPages}
-
                     // @ts-ignore
                     pokemons={sortedPokemons}/>
-                <Pagination totalPages={totalPages} page={page} setPage={setPage}/>
             </main>
         </div>
     );
