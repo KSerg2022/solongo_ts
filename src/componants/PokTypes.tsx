73c42a738632e9a83d14c8085fad790d65cf9514
KSerg2022 <114860122+KSerg2022@users.noreply.github.com>
2023-11-25 11:04:39 +0100

    refactored

--- src/componants/PokTypes.tsx
+++ src/componants/PokTypes.tsx
@@ -1,14 +1,7 @@
-import React, {useState} from 'react';
-
-import {useDispatch} from "react-redux"
-import {setFilters} from "../redux/filters/actionsFilters"
-import {useTypesSelector} from '../hooks/useTypedSelector';
+import React from 'react';
 import {MyLi} from "./UI/MiLi/MyLi"
 
 export const PokTypes = ({types}: any) => {
-    const dispatch = useDispatch();
-    const {filters} = useTypesSelector(state => state.filters)
-
 
     return (
         <ul className="typesList">
